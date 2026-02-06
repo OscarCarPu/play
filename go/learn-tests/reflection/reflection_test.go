39a9df8d278ca08579a271188938ff3f6055c756
OCP <oscarcarballopuebla@gmail.com>
2026-02-06 10:09:47 +0100

    feat: add reflection package with walk function and tests
    * Implement `getValue` function to get the reflect value of an interface
    * Implement `walk` function to recursively walk through the fields of a struct, slice, array, map, chan, or func
    * Add tests for `walk` function with different types of input, including structs, slices, arrays, maps, chans, and funcs
    * Test cases cover various scenarios, including nested structs, pointers, and different types of fields
    * Use `reflect` package to handle different types of data and `testing` package to write unit tests

---
+++ go/learn-tests/reflection/reflection_test.go
@@ -0,0 +1,155 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to coantain %q but it didn't", haystack, needle)
+	}
+}
+
+func TestWalk(t *testing.T) {
+	type Profile struct {
+		Age  int
+		City string
+	}
+	type Person struct {
+		Name    string
+		Profile Profile
+	}
+
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"nested struct",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"pointers",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{44, "Paris"},
+			},
+			[]string{"London", "Paris"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{44, "Paris"},
+			},
+			[]string{"London", "Paris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+	t.Run("maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Chris": "London",
+			"John":  "Paris",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "London")
+		assertContains(t, got, "Paris")
+	})
+	t.Run("channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{44, "London"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "London"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+	t.Run("func", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{44, "London"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "London"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
